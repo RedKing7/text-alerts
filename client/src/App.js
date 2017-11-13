a44764869a65f2d5e0e318b6ecdde2601f6d0748
Rory Johnson <redkjohnson@gmail.com>
2017-11-13 12:58:34 -0500

    testing text on heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class App extends Component {
+
+  test_twilio = async () => {
+    try {
+      let response = await axios.get('/test')
+      console.log(response.data)
+    } catch (err) { console.log(err) }
+  }
+
   render() {
     return (
       <div>
         <h1>Hello from React!</h1>
+        <button onClick={this.test_twilio}>test</button>
       </div>
     );
   }
