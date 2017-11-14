036518005f0a22c5132e92d78a99d4106ecc169e
Rory Johnson <redkjohnson@gmail.com>
2017-11-14 12:52:49 -0500

    alarms now send text at their time

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -1,6 +1,37 @@
 class Alarm < ApplicationRecord
   belongs_to :user
-  validates :name, :time_of_alarm, :repeat, presence: true
+  validates :name, :time_of_alarm, presence: true
 
-  # after_create
+  after_create :send_reminder
+  before_create :confirmation
+
+  def send_reminder
+    @phone_number = User.find(user_id).phone_number
+    puts @phone_number
+    alarm = "#\n#{name}"
+    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+    message = @client.messages.create(
+      :from => TWILIO_NUMBER,
+      :to => @phone_number,
+      :body => alarm
+    )
+  end
+
+  def when_to_run
+    time_of_alarm
+  end
+
+  handle_asynchronously :send_reminder, :run_at => proc { |i| i.when_to_run }
+
+  def confirmation
+    time_str = ((time_of_alarm).localtime).strftime("%I:%M%p on %b. %d, %Y")
+    @phone_number = User.find(user_id).phone_number
+    puts @phone_number
+    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+    message = @client.messages.create(
+      body: "\nYou scheduled an alarm for #{time_str}.",
+      to: @phone_number,
+      from: TWILIO_NUMBER
+    )
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ rory.password = 'password'
 rory.phone_number = TEST_NUMBER
 rory.save
 
-alert_time = 2.minutes.from_now # Time.zone.now.to_datetime
+alert_time = 1.minutes.from_now # Time.zone.now.to_datetime
 
 Alarm.create({user_id: rory.id, name: 'test alarm', repeat: false, time_of_alarm: alert_time})
-Reminder.create({user_id: rory.id, title: 'test reminder', task: 'test message', time_of_reminder: alert_time})
+# Reminder.create({user_id: rory.id, title: 'test reminder', task: 'complete [test] task', time_of_reminder: alert_time})
