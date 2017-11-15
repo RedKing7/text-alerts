7e0a82a9d09460a45b1962ff45b9ce86c3892754
Rory Johnson <redkjohnson@gmail.com>
2017-11-15 12:41:00 -0500

    removed user index route, added valiidations for name, number, and secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :phone_number, presence: true
+
   has_many :alarms, dependent: :destroy
   has_many :reminders, dependent: :destroy
 end
